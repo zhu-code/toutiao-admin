94d9cd08d9e19513ef17c9266154f8d235f5cad6
zhu-code <826486452@qq.com>
2020-11-29 17:49:44 +0800

    ''

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//在全局中加载element-ui组件库
+import ElementUI from 'element-ui'
+//在全局中加载element-ui组件库的样式
+import 'element-ui/lib/theme-chalk/index.css'
+//全局注册element-ui组件
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
